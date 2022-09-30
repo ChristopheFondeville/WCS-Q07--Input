8f42822286c6e357f357d436f60bb8b8f0de3606
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-09-30 09:34:58 +0200

    Shit

--- src/app/models/developer.model.ts
+++ src/app/models/developer.model.ts
@@ -1,4 +1,13 @@
 export class DeveloperModel {
+  constructor(
+    lastName: string,
+    firstName: string,
+    age: number,
+    gender: string,
+    bio: string,
+    skills: string
+  ) {
+  }
 
 
 }
--- src/app/models/skil.model.ts
+++ src/app/models/skil.model.ts
@@ -1,4 +1,8 @@
 export class SkilModel{
-
-
+  constructor(
+    name: string,
+    logo: string,
+    site: string
+  ) {
+  }
 }
